6af184db7ef9a667be8ec8f5aba484ca3f5c5878
FireWarri <sebastianhultin@users.noreply.github.com>
2017-11-01 20:48:25 +0100

    Initial commit

---
+++ desktop/src/se/dungeonboys/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.dungeonboys.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.dungeonboys.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
